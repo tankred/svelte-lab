fea93f6b5ec5e21012d9ddc4f6a00bd52235337f
Kurt Duyck <kurt.duyck@vives.be>
2020-05-12 13:09:40 +0200

    Update cSS Css
    	modified:   CHANGELOG.md
    	modified:   src/App.svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.3] - 2020-05-12
+### Changed
+- Add paragraph, and modify styles
+
 ## [0.1.2] - 2020-05-11
 ### Changed
 - Ignore * swp * files
--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,13 @@
 	<h1>Hello {name}!</h1>
 	<Counter />
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+  body {
+    background-color: #0f0;
+  }
 	main {
 		text-align: center;
 		padding: 1em;
@@ -19,7 +23,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
