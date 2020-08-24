159a0240a0dabdb5763f1feb6c2aaecaeb90901a
Kurt Duyck <kurt.duyck@vives.be>
2020-08-24 12:47:33 +0200

    Add radio buttons
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   src/App.svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.26] - 2020-08-24
+### Changed
+- Radio buttons input
+
 ## [0.1.25] - 2020-07-14
 ### Changed
 - Add checkbox input
--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,13 @@
   // export
   export let name;
   // let
+  let scoops = 1;
+  let flavours = ['Mint choc chip'];
+  let menu = [
+	'Cookies and cream',
+	'Mint choc chip',
+	'Raspberry ripple'
+  ];
   let yes = false;
   let a = 1;
   let b = 2;
@@ -32,6 +39,10 @@
   let promise = getRandomNumber();
 
   // function
+  function join(flavours) {
+    if (flavours.length === 1) return flavours[0];
+    return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
+  }
   function handleMessage(event) {
 		alert(event.detail.text);
   }
@@ -75,6 +86,36 @@
 <main>
   <div id="container">
 	<h1>Hello {name}!</h1>
+	<h2>Size</h2>
+	<label>
+	  <input type=radio bind:group={scoops} value={1}>
+  	  One scoop
+	</label>
+	<label>
+	<input type=radio bind:group={scoops} value={2}>
+	Two scoops
+	</label>
+	<label>
+	<input type=radio bind:group={scoops} value={3}>
+	Three scoops
+	</label>
+	<h2>Flavours</h2>
+	{#each menu as flavour}
+	<label>
+		<input type=checkbox bind:group={flavours} value={flavour}>
+		{flavour}
+	</label>
+	{/each}
+	{#if flavours.length === 0}
+	<p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+	<p>Can't order more flavours than scoops!</p>
+{:else}
+	<p>
+		You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+		of {join(flavours)}
+	</p>
+{/if}
 	<label>
 	<input type=checkbox bind:checked={yes}>
 	Yes! Send me regular email spam
