529728b3aa2a21fb6c618637f7986964b36e0f84
Kurt Duyck <kurt.duyck@vives.be>
2020-10-13 10:10:11 +0200

    Fixed CSS in rollup.config
    	modified:   .gitignore
    	deleted:    public/favicon.png
    	deleted:    public/global.css
    	modified:   rollup.config.js
    	modified:   src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,9 @@
-<script>
-	import Counter from "./Counter.svelte";
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<Counter />
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-  body {
-    background-color: #0f0;
-  }
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
 	}
 </style>
+
+<p>This is a paragraph.</p>
