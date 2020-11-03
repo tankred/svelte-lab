1b0f664c90abbd8cf4bcabd83bf7cbb13732b0d4
Kurt Duyck <kurt.duyck@vives.be>
2020-11-03 21:47:46 +0100

    Implement single dynamic component
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   src/App.svelte
    	new file:   src/BlueThing.svelte
    	new file:   src/GreenThing.svelte
    	new file:   src/RedThing.svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.5.12] - 2020-11-03
+### Added
+- Files and folders.svelte
+
 ## [0.5.11] - 2020-11-02
 ### Added
 - Files and folders.svelte
--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,21 @@
 <script>
-	import Folder from './Folder.svelte';
+	import RedThing from './RedThing.svelte';
+	import GreenThing from './GreenThing.svelte';
+	import BlueThing from './BlueThing.svelte';
 
-	let root = [
-		{
-			type: 'folder',
-			name: 'Important work stuff',
-			files: [
-				{ type: 'file', name: 'quarterly-results.xlsx' }
-			]
-		},
-		{
-			type: 'folder',
-			name: 'Animal GIFs',
-			files: [
-				{
-					type: 'folder',
-					name: 'Dogs',
-					files: [
-						{ type: 'file', name: 'treadmill.gif' },
-						{ type: 'file', name: 'rope-jumping.gif' }
-					]
-				},
-				{
-					type: 'folder',
-					name: 'Goats',
-					files: [
-						{ type: 'file', name: 'parkour.gif' },
-						{ type: 'file', name: 'rampage.gif' }
-					]
-				},
-				{ type: 'file', name: 'cat-roomba.gif' },
-				{ type: 'file', name: 'duck-shuffle.gif' },
-				{ type: 'file', name: 'monkey-on-a-pig.gif' }
-			]
-		},
-		{ type: 'file', name: 'TODO.md' }
+	const options = [
+		{ color: 'red',   component: RedThing   },
+		{ color: 'green', component: GreenThing },
+		{ color: 'blue',  component: BlueThing  },
 	];
+
+	let selected = options[0];
 </script>
 
-<Folder name="Home" files={root} expanded/>
+<select bind:value={selected}>
+	{#each options as option}
+		<option value={option}>{option.color}</option>
+	{/each}
+</select>
+
+<svelte:component this={selected.component}/>
