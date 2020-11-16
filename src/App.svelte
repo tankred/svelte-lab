7e4d54b3e842701a88b84123f9d4b849e308835b
Kurt Duyck <kurt.duyck@vives.be>
2020-11-16 11:10:20 +0100

    Implement dark theme
    	modified:   CHANGELOG.md
    	modified:   package.json
    	new file:   public/dark-theme.css
    	modified:   src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,6 @@
-<script>
-	let hereKitty = false;
+<svelte:head>
+	<link rel="stylesheet" href="dark-theme.css">
+</svelte:head>
 
-	const handleMouseenter = () => hereKitty = true;
-	const handleMouseleave = () => hereKitty = false;
-
-</script>
-
-<style>
-	img {
-		position: absolute;
-		left: 0;
-		bottom: -60px;
-		transform: translate(-80%, 0) rotate(-30deg);
-		transform-origin: 100% 100%;
-		transition: transform 0.4s;
-	}
-
-	.curious {
-		transform: translate(-15%, 0) rotate(0deg);
-	}
-
-	:global(body) {
-		overflow: hidden;
-	}
-</style>
-
-<svelte:body
-	on:mouseenter={handleMouseenter}
-	on:mouseleave={handleMouseleave}
-/>
-
-<!-- creative commons BY-NC http://www.pngall.com/kitten-png/download/7247 -->
-  <!-- ADDED kitten to public dir --> 
-<img
-	class:curious={hereKitty}
-	alt="Kitten wants to know what's going on"
-	src="kitten.png"
->
+<h1>Hello world </h1>
+<p> Om het dark theme mee te compilen: put dark-theme.css in public dir </p>
