73277da1d1de1f4e2996d101732c6423012157e2
Kurt Duyck <kurt.duyck@vives.be>
2020-11-24 21:05:44 +0100

    Congratulations with confetti
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   src/App.svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.5.21] - 2020-11-24
+### Changed
+- Congratulations with confetti 
+
 ## [0.5.20] - 2020-11-23
 ### Changed
 - Debug script
--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,49 @@
 <script>
-	let user = {
-		firstname: 'Ada',
-		lastname: 'Lovelace'
-	};
+	import { onMount } from 'svelte';
+
+	let characters = ['🥳', '🎉', '✨'];
+
+	let confetti = new Array(100).fill()
+		.map((_, i) => {
+			return {
+				character: characters[i % characters.length],
+				x: Math.random() * 100,
+				y: -20 - Math.random() * 100,
+				r: 0.1 + Math.random() * 1
+			};
+		})
+		.sort((a, b) => a.r - b.r);
+
+	onMount(() => {
+		let frame;
+
+		function loop() {
+			frame = requestAnimationFrame(loop);
+
+			confetti = confetti.map(emoji => {
+				emoji.y += 0.7 * emoji.r;
+				if (emoji.y > 120) emoji.y = -20;
+				return emoji;
+			});
+		}
+
+		loop();
+
+		return () => cancelAnimationFrame(frame);
+	});
 </script>
 
-<input bind:value={user.firstname}>
-<input bind:value={user.lastname}>
+<style>
+	:global(body) {
+		overflow: hidden;
+	}
 
-{@debug user}
+	span {
+		position: absolute;
+		font-size: 5vw;
+	}
+</style>
 
-<h1>Hello {user.firstname}!</h1>
+{#each confetti as c}
+	<span style="left: {c.x}%; top: {c.y}%; transform: scale({c.r})">{c.character}</span>
+{/each}
